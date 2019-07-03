e45e83e08b6f8478f21d536b203b1f84cd83ba96
Matt Levy <milevy1@gmail.com>
2019-07-03 15:23:20 -0600

    Adds Heroku configuration for production db and dotenv for variable privacy
    Co-authored-by: Jennica Stiehl <25069483+stiehlrod@users.noreply.github.com>

--- knexfile.js
+++ knexfile.js
@@ -29,17 +29,16 @@ module.exports = {
 
   production: {
     client: 'pg',
-    connection: {
-      database: 'tracker_production',
-      user:     'jennicastiehl',
-      password: 'null'
-    },
+    connection: process.env.DATABASE_URL,
     pool: {
       min: 2,
       max: 10
     },
     migrations: {
-      tableName: 'knex_migrations'
+      directory: __dirname + '/db/migrations',
+    },
+    seeds: {
+      directory: __dirname + '/db/seeds/production',
     }
   }
 
