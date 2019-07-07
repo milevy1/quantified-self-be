d9d3e1f9d911731b1c15121973475d1ff51054ba
JennicaStiehl <jennica.stiehl@gmail.com>
2019-07-07 14:05:14 -0600

    adds success with Objection, eager loading and the meals with foods route

--- routes/api/v1/meals.js
+++ routes/api/v1/meals.js
@@ -1,61 +1,33 @@
-const pry = require('pryjs');
+// const pry = require('pryjs');
 
 var express = require('express');
 var router = express.Router();
-
 // Knex connection to database
 const environment = process.env.NODE_ENV || "development";
 const configuration = require("../../../knexfile")[environment];
 const database = require("knex")(configuration);
-
+//objection
 const Food = require('../../../models/food');
 const Meal = require('../../../models/meal');
 const { Model } = require('objection');
 Model.knex(database)
+
 /* GET meals */
 router.get('/', async (request, response) => {
   try {
     const meals = await Meal
-      .query()
-      .eager('foods');
+    .query()
+    .select('name')
+    .eager('foods(selectNameAndCalories)', {
+      selectNameAndCalories: function(builder){
+        builder.select('name','calories')
+      }
+    })
     response.send(meals)
-    }
-    catch (error) {
-      // eval(pry.it)
-      response.status(404).json({ error })
-    }
+
+  } catch(error) {
+    response.status(404).json({ error });
+  }
 });
-// router.get('/', (req, res) => {
-//     Meal.query()
-//         .then(meals => {
-//             res.json(meals)
-//         })
-// })
-//-------------
-// router.get('/', function(req, res, next) {
-//   database('meals').select('id', 'name')
-//     .then((meals) => {
-//       res.status(200).json(meals);
-//     })
-//     .catch((error) => {
-//       res.status(500).json({ error });
-//     });
-// });
-//-----------------------
-// const objection = require('objection');
-// const Food = require('../../../models/food');
-// const Meal = require('../../../models/meal');
-// router.get('/', async (req, res) => {
-  // try {
-    // const meals = await Meal
-      // .query()
-      // .eager('foods');
-    // res.send(meals)
-    //
-    // }
-    // catch (error) {
-    //   res.status(404).json({ error })
-    // }
-// });
 
 module.exports = router;
