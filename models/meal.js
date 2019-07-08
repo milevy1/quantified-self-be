eb8cdd4698c4b2c31e7b72c2bc0f88a92c2aa10a
JennicaStiehl <jennica.stiehl@gmail.com>
2019-07-08 16:49:10 -0600

    adds routes for meal_foods (post,delete)

---
+++ models/meal.js
@@ -0,0 +1,31 @@
+const knex = require('knex')({client: 'pg'})
+const connection = require('../knexfile')
+const { Model } = require('objection')
+const knexConnection = knex(connection)
+Model.knex(knexConnection)
+
+class Meal extends Model {
+  static get tableName () {
+    return 'meals'
+  }
+  static get relationMappings () {
+    const Food = require('./food')
+
+    return {
+      foods: {
+        relation: Model.ManyToManyRelation,
+        modelClass: Food,
+        join: {
+          from: 'meals.id',
+          through: {
+            from: 'meal_foods.meal_id',
+            to: 'meal_foods.food_id'
+          },
+          to: 'foods.id'
+        }
+      }
+    }
+  }
+}
+
+module.exports = Meal;
--- routes/api/v1/meals.js
+++ routes/api/v1/meals.js
@@ -1,35 +1,79 @@
+const pry = require('pryjs');
+// eval(pry.it)
+
 var express = require('express');
 var router = express.Router();
-
 // Knex connection to database
-const environment = process.env.NODE_ENV || 'development';
-const configuration = require('../../../knexfile')[environment];
-const database = require('knex')(configuration);
+const environment = process.env.NODE_ENV || "development";
+const configuration = require("../../../knexfile")[environment];
+const database = require("knex")(configuration);
+//objection
+const Food = require('../../../models/food');
+const Meal = require('../../../models/meal');
+const MealFood = require('../../../models/meal_food');
+const { Model } = require('objection');
+Model.knex(database)
 
-/* GET foods */
-router.get('/', function(req, res, next) {
-  database('meals').select('id', 'name')
-    .then((meals) => {
-      res.status(200).json(meals);
+/* GET meals */
+router.get('/', async (request, response) => {
+  try {
+    const meals = await Meal
+    .query()
+    .select('id', 'name')
+    .eager('foods(selectNameAndCalories)', {
+      selectNameAndCalories: function(builder){
+        builder.select('name','calories')
+      }
     })
-    .catch((error) => {
-      res.status(500).json({ error });
-    });
-});
-// const objection = require('objection');
-// const Food = require('../../../models/food');
-// const Meal = require('../../../models/meal');
-// router.get('/', async (req, res) => {
-  // try {
-    // const meals = await Meal
-      // .query()
-      // .eager('foods');
-    // res.send(meals)
-    //
-    // }
-    // catch (error) {
-    //   res.status(404).json({ error })
-    // }
-// });
+    response.send(meals)
 
+  } catch(error) {
+    response.status(404).json({ error });
+  }
+});
+/* GET meal by id and assoc foods */
+router.get('/:meal_id/foods', async (request, response) => {
+  const id_param = parseInt(request.params.meal_id)
+  try {
+    const meals = await Meal
+    .query()
+    .select('id', 'name')
+    .where('id', id_param)
+    .eager('foods(selectNameAndCalories)', {
+      selectNameAndCalories: function(builder){
+        builder.select('name','calories')
+      }
+    })
+    response.send(meals)
+
+  } catch(error) {
+    response.status(404).json({ error });
+  }
+});
+/* POST food with assoc meal */
+router.post('/:meal_id/foods/:id', async(req, res) => {
+try {
+  const food= await Food.query().findById(req.params.id);
+  const meal = await Meal.query().findById(req.params.meal_id);
+  const wait = await food.$relatedQuery('meals').relate(meal.id);
+  res.send('message: Successfully added food.name'+' to '+ 'meal.name')
+}
+    catch(error) {
+      res.status(500).json({ error });
+    }
+});
+//delete a meal_foods record
+router.delete('/:meal_id/foods/:id', async (req, res) => {
+  try {
+  const numDeleted = await MealFood
+    .query()
+    .delete()
+    .where("food_id", parseInt(req.params.id))
+    .where("meal_id", parseInt(req.params.meal_id));
+  res.status(204).send();
+}
+    catch(error) {
+      res.status(404).json({ error });
+    }
+});
 module.exports = router;
