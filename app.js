ff9003e15853039cdbeb5a1214b4631f86490e3c
JennicaStiehl <jennica.stiehl@gmail.com>
2019-07-08 17:08:06 -0600

    adds configuration, npm i objection & pryjs

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
-require('dotenv').config();
-
+require('dotenv').config()
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
@@ -7,8 +6,18 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var foodsRouter = require('./routes/api/v1/foods');
+var mealsRouter = require('./routes/api/v1/meals');
 
 var app = express();
+//knex & objection---
+const Knex = require('knex');
+const { Model } = require('objection');
+const knexConfig = require('./knexfile');
+const knex = Knex(knexConfig);
+Model.knex(knex);
+
+const port = process.env.PORT || 5000
+//knex & objection---
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -18,5 +27,17 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/api/v1/foods', foodsRouter);
+app.use('/api/v1/meals', mealsRouter);
+
+app.listen(port, () => {
+	console.log('Listening on port: ' + port)
+})
+
+const Meal = require('./models/meal');
+const environment = process.env.NODE_ENV || "development";
+const configuration = require("./knexfile")[environment];
+const database = require("knex")(configuration);
+
+Model.knex(database)
 
 module.exports = app;
